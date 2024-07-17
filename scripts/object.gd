ab4d544d35c20d77edc7dff57f5fd3a80c9812be
andy ramiandrisoa <andy.ramiand@gmail.com>
2024-07-17 18:29:28 +0300

    Initial commit

---
+++ scripts/object.gd
@@ -0,0 +1,28 @@
+extends StaticBody2D
+var isHold = false
+var playerPosition
+var objectPosition = $".".get_position
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(isHold, playerPosition)
+	if Input.is_action_pressed("ui_space"):
+		isHold = not isHold
+		print("Space pressed")
+	#if isHold == true: 
+		#$".".global_position = playerPosition + Vector2(5 , 10)
+
+func _on_interaction_body_entered(body):
+	$Label.visible = true
+
+
+func _on_interaction_body_exited(body):
+	$Label.visible = false
+	playerPosition = body.get_position()
+
---
+++ scripts/player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody2D
+
+const SPEED = 100
+var current_SPEED = SPEED
+const factor_SPEED_acc = 1.8
+
+func _ready():
+	$AnimatedSprite2D.play("idle")
+	
+	
+func _physics_process(delta):
+	player_movement(delta)
+	
+
+func player_movement(delta):
+	var x_axis = Input.get_axis("ui_left" , "ui_right")
+	var y_axis = Input.get_axis("ui_up" , "ui_down")
+	var acceleration = Input.is_key_pressed(KEY_SHIFT)
+	
+	if acceleration == true : 
+		current_SPEED = SPEED * factor_SPEED_acc
+	else :
+		current_SPEED = SPEED
+
+	if x_axis != 0 and y_axis ==0 : 
+		velocity = Vector2(current_SPEED * x_axis , 0)
+	elif x_axis == 0 and y_axis != 0 :
+		velocity = Vector2(0 , current_SPEED * y_axis )
+	elif x_axis != 0 and y_axis != 0 :
+		velocity = Vector2((current_SPEED / sqrt(2)) * x_axis, (current_SPEED / sqrt(2)) * y_axis)
+	else : 
+		velocity = Vector2(0,0)
+	play_anim(x_axis , y_axis, acceleration)
+	move_and_slide()	
+
+func isFliped(x_axis):
+	if x_axis >= 0 :
+		return false
+	else :
+		return true 
+
+func play_anim(x_axis , y_axis, acceleration) :
+	var anim = $AnimatedSprite2D
+	if x_axis == 0 and y_axis ==0 :
+		anim.play("idle")
+	else : 
+		anim.flip_h = isFliped(x_axis)
+		if acceleration == false :
+			anim.play("walk")
+		else :
+			anim.play("run")
+		
+		
